808f5fc9e7de43360514367d370a81c1f48ddf25
Ashis Biswas <ashis@tomarweb.in>
2024-12-11 23:58:47 +0530

    Project Updated

---
+++ module-example/iife.js
@@ -0,0 +1,9 @@
+(function (message) {
+    const superHero = "BatMan";
+    console.log(message, superHero);
+})("Hey! ");
+
+(function (message) {
+    const superHero = "SuperMan";
+    console.log(message, superHero);
+})("Hello! ");
--- module-example/index.js
+++ module-example/index.js
@@ -1,2 +1,8 @@
-require('./add')
+const add = require('./add')
+
 console.log("Hello from moudule-example")
+const sum1 = add(1,2)
+const sum2 = add(2,3)
+
+console.log(sum1)
+console.log(sum2)
